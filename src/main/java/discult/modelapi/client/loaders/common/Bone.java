c94db571732767118d929d2f60db3aa9390c1104
Jordan Ellison <jordanellison863@gmail.com>
2019-07-31 02:03:31 +0100

    adding math utils and the start to bone and skeleton

--- src/main/java/discult/modelapi/client/loaders/common/Bone.java
+++ src/main/java/discult/modelapi/client/loaders/common/Bone.java
@@ -1,5 +1,75 @@
 package discult.modelapi.client.loaders.common;
 
+import discult.modelapi.utils.math.Matrix4;
+import discult.modelapi.utils.math.Vec4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Bone
 {
+    private final int ID;
+    private final String name;
+    private final Bone parent;
+
+    private Vec4 position;
+    private Matrix4 bindMatrix = new Matrix4();
+    private Matrix4 transformMatrix;
+    private Matrix4 inverseBindMatrix = new Matrix4();
+
+    //TODO check if this can actually be final.
+    public final List<Bone> children = new ArrayList<>();
+    public final Map<String, Map<Integer, Matrix4>> animations = new HashMap<>();
+    public final Map<Vertex, Float> verts = new HashMap<>();
+
+
+    public Bone(int ID, String name, Bone parent)
+    {
+        this.ID = ID;
+        this.name = name;
+        this.parent = parent;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Bone getParent()
+    {
+        return parent;
+    }
+
+    public Vec4 getPosition()
+    {
+        return position;
+    }
+
+    public Matrix4 getTransformMatrix()
+    {
+        return transformMatrix;
+    }
+
+    public void setBindMatrix(Matrix4 bindMatrix)
+    {
+        this.bindMatrix = bindMatrix;
+        setInverseBindMatrix(bindMatrix);
+    }
+
+    private void setInverseBindMatrix(Matrix4 bindMatrix)
+    {
+        this.inverseBindMatrix = Matrix4.invert(bindMatrix, this.inverseBindMatrix);
+    }
+
+    private void calculateModified()
+    {
+
+    }
 }
--- src/main/java/discult/modelapi/utils/math/Vec4.java
+++ src/main/java/discult/modelapi/utils/math/Vec4.java
@@ -2,4 +2,34 @@ package discult.modelapi.utils.math;
 
 public class Vec4
 {
+
+    public float x,y,z,w;
+
+    public Vec4() {}
+
+    public Vec4(float x, float y, float z, float w)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public static Vec4 transform(Matrix4 matrix, Vec4 vec4, Vec4 out)
+    {
+        if(out == null)
+            out = new Vec4();
+
+       out.x = matrix.m00 * vec4.x + matrix.m10 * vec4.y + matrix.m20 * vec4.z + matrix.m30 * vec4.w;
+       out.y = matrix.m01 * vec4.x + matrix.m11 * vec4.y + matrix.m21 * vec4.z + matrix.m31 * vec4.w;
+       out.z = matrix.m02 * vec4.x + matrix.m12 * vec4.y + matrix.m22 * vec4.z + matrix.m32 * vec4.w;
+       out.w = matrix.m03 * vec4.x + matrix.m13 * vec4.y + matrix.m23 * vec4.z + matrix.m33 * vec4.w;
+
+       return out;
+    }
+
+    public void transform(Matrix4 matrix, Vec4 vec4)
+    {
+        transform(matrix, vec4, this);
+    }
 }
